1662fbdc4afa695ab2aefaa3306e2b329d250ae3
Omer Gronich <gronichomer@gmail.com>
2022-04-30 01:47:15 +0300

    :green_heart: ci: added CI job for PRS & Also fixed all tests

--- libs/front-end/board/features/board/src/lib/board.component.spec.ts
+++ libs/front-end/board/features/board/src/lib/board.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BoardComponent } from './board.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BoardFacade } from '@budgetello/front-end/board/domain';
+
+const createBoardFacadeMock = () => ({
+  destroy: jest.fn(),
+  init: jest.fn(),
+});
 
 describe('BoardComponent', () => {
   let component: BoardComponent;
@@ -8,7 +15,9 @@ describe('BoardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [BoardComponent],
+      providers: [{ provide: BoardFacade, useValue: createBoardFacadeMock() }],
     }).compileComponents();
   });
 
--- libs/front-end/home/features/home/src/lib/home.component.spec.ts
+++ libs/front-end/home/features/home/src/lib/home.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HomeFacade } from '@budgetello/front-end/home/domain';
+
+class HomeFacadeMock {
+  destroy = jest.fn();
+  init = jest.fn();
+}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +15,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      providers: [{ provide: HomeFacade, useClass: HomeFacadeMock }],
+      declarations: [HomeComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
