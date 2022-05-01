365712d21b2a968cfdcecf4e06197cede1360b70
Omer Gronich <gronichomer@gmail.com>
2022-05-01 21:31:08 +0300

    :white_check_mark: test(ui): added test to board preview component in home domain

--- libs/front-end/home/ui/board-preview/src/lib/board-preview.component.spec.ts
+++ libs/front-end/home/ui/board-preview/src/lib/board-preview.component.spec.ts
@@ -1,24 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BoardPreviewComponent } from './board-preview.component';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { BOARD_TITLE } from './tests/mocks';
+import { By } from '@angular/platform-browser';
 
 describe('BoardPreviewComponent', () => {
-  let component: BoardPreviewComponent;
-  let fixture: ComponentFixture<BoardPreviewComponent>;
+  @Component({
+    template:
+      '<budgetello-board-preview [boardTitle]="boardTitle"></budgetello-board-preview>',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
+  class TestHostComponent {
+    @Input() boardTitle: string;
+  }
+
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [BoardPreviewComponent],
+      declarations: [TestHostComponent, BoardPreviewComponent],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BoardPreviewComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
+    component.boardTitle = BOARD_TITLE;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the board title', () => {
+    const boardTitle = fixture.debugElement.query(
+      By.css('[data-test="board-title"]')
+    );
+    expect(boardTitle.nativeElement.textContent).toBe(BOARD_TITLE);
   });
 });
